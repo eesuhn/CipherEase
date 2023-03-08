777a763a23ebc1734d3b05b09ecb988adde611a6
eesuhn <102596628+eesuhn@users.noreply.github.com>
2023-03-08 20:42:14 +0800

    Added validation in control, Added Bootstrap for index

--- control.php
+++ control.php
@@ -2,4 +2,49 @@
     include 'backend/cipher.php';
     include 'backend/caesar.php';
     include 'backend/vigenere.php';
+
+    function encryptChoice ($choiceCipher, $input, $key, $choiceCrypt) {
+
+        if (empty($input) && empty($key)) {
+            return "";
+        }
+
+        if (!validateInput($input)) {
+            return "Invalid input. Please enter only alphabetic characters.";
+        }
+
+        switch ($choiceCipher) {
+            case 'caesar':
+                if (!validateKey_Caesar($key)) {
+                    return "Invalid key. Please enter only numeric characters.";
+                }
+
+                $cipher = new Caesar($input, $key);
+                if ($choiceCrypt == "encrypt") {
+                    $cipher->encrypt();
+                } else {
+                    $cipher->decrypt();
+                }
+                return $cipher->getOutput();
+                break;
+        }
+    }
+    function validateInput ($input) {
+        if (empty($input)) {
+            return false;
+        }
+        if (!ctype_alpha(str_replace(' ', '', $input))) {
+            return false;
+        }
+        return true;
+    }
+    function validateKey_Caesar ($key) {
+        if (empty($key)) {
+            return false;
+        }
+        if (!ctype_digit($key)) {
+            return false;
+        }
+        return true;
+    }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,76 @@
 <?php
     include 'control.php';
 ?>
+<html>
+    <head>
+        <title>Cipher Ease</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    </head>
+    
+    <body>
+        <div class="h-100 d-flex justify-content-center align-items-center">
+            <form action="<?=basename($_SERVER["PHP_SELF"])?>" method="POST">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Cipher with Ease</h2>
+                    </div>
+                </div>
+
+                <div class="row" style="padding: 10px 0 0;">
+                    <div class="col-md-12">
+                        <label>Input: </label>
+                        <input type="text" name="input" class="form-control" value="<?php echo $_POST["input"] ?? ""; ?>">
+                    </div>
+
+                    <div class="col-md-12">
+                        <label style="padding: 10px 0 0;">Key: </label>
+                        <input type="text" name="key" class="form-control" value="<?php echo $_POST["key"] ?? ""; ?>">
+                    </div>
+                </div>
+
+                <div class="row" style="padding: 30px 0 0;">
+                    <div class="col-md-6">
+                        <select name="choiceCipher" class="form-control">
+                            <option value="caesar"
+                                <?php
+                                    if (isset($_POST["choiceCipher"])) {
+                                        if ($_POST["choiceCipher"] == "caesar") {
+                                            echo "selected";
+                                        }
+                                    }
+                                ?>
+                            >Caesar</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-3">
+                        <input type="submit" name="encrypt" class="btn btn-primary" value="Encrypt">
+                    </div>
+                    <div class="col-md-3">
+                        <input type="submit" name="decrypt" class="btn btn-primary" value="Decrypt">
+                    </div>
+                </div>
+
+                <div class="row" style="padding: 20px 0 0;">
+                    <div class="col-md-12">
+                        <label>Output: </label>
+                        <textarea class="form-control" style="resize: none;" readonly><?php
+                            if (isset($_POST["encrypt"]) || isset($_POST["decrypt"])) {
+
+                                $choiceCrypt = isset($_POST["encrypt"]) ? "encrypt" : "decrypt";
+
+                                $input = $_POST["input"];
+                                $key = $_POST["key"];
+                                $choiceCipher = $_POST["choiceCipher"];
+
+                                echo encryptChoice($choiceCipher, $input, $key, $choiceCrypt);
+                            }
+                        ?></textarea>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </body>
+</html>
