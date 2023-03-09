61fecad247dd239e9434b2a80c8444fa15bf2433
Alexander <88991975+ElongatedTusk@users.noreply.github.com>
2023-03-09 20:26:49 +0800

    columnar.php now writes by row & reads by column
    Moved original contents of this file to backend/columnar_rw_row.php

--- backend/columnar.php
+++ backend/columnar.php
@@ -1,5 +1,4 @@
 <?php
-
 class Columnar {
     private $text;
     private $key;
@@ -7,52 +6,59 @@ class Columnar {
     private $cols;
     private $table;
     private $output;
-    
+
     public function __construct($text, $key) {
         $this->text = $text;
         $this->key = $key;
         $this->output = "";
         $this->table = array();
     }
-    
+
     public function encrypt() {
         $this->generateTable();
         $this->sortTable();
         $this->readTable();
     }
-    
+
     public function decrypt() {
         $this->generateTable();
         $this->sortTable();
         $this->readTable();
     }
-    
+
     public function getOutput() {
         return $this->output;
     }
-    
+
     private function generateTable() {
         $this->rows = ceil(strlen($this->text) / strlen($this->key));
         $this->cols = strlen($this->key);
         $this->text = str_pad($this->text, $this->rows * $this->cols, " ");
+        $this->table = array();
         for ($i = 0; $i < $this->cols; $i++) {
-            $this->table[$i] = "";
+            $this->table[$i] = str_repeat(' ', $this->rows);
             for ($j = 0; $j < $this->rows; $j++) {
-                $this->table[$i] .= $this->text[$j * $this->cols + $i];
+                $this->table[$i][$j] = $this->text[$j * $this->cols + $i];
             }
         }
     }
-    
+
     private function sortTable() {
-    $keys = str_split($this->key);
-    array_multisort($keys, SORT_ASC, SORT_STRING, $this->table);
-}
-    
+        $keys = str_split($this->key);
+        $key_indexes = range(0, strlen($this->key)-1);
+        array_multisort($keys, SORT_ASC, SORT_STRING, $key_indexes);
+        $sorted_table = array();
+        foreach ($key_indexes as $i) {
+            $sorted_table[] = $this->table[$i];
+        }
+        $this->table = $sorted_table;
+    }
+
     private function readTable() {
         $this->output = "";
-        for ($i = 0; $i < $this->rows; $i++) {
-            for ($j = 0; $j < $this->cols; $j++) {
-                $this->output .= $this->table[$j][$i];
+        for ($i = 0; $i < $this->cols; $i++) {
+            for ($j = 0; $j < $this->rows; $j++) {
+                $this->output .= $this->table[$i][$j];
             }
         }
     }
